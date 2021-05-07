284e6cd5a616b24f3e9b9c499372110c61ae5549
Lucira Silva <luciracs0@gmail.com>
2021-05-07 09:37:07 -0300

    style: App area turn flex

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Component } from "react";
+import Message from "./components/Message";
+import Button from "./components/Button";
+import "./styles/App.css";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+      <Message>Bem-vindo(a) ao torneio tribruxo!</Message>
+      <Button>iniciar jogo</Button>
       </div>
     );
   }
